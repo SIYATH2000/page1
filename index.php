0aa0b24a51587929dd97052239583937042ac6ea
MOHAMED SIYATH <87572745+SIYATH2000@users.noreply.github.com>
2022-08-12 22:59:43 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+    <head>
+        <title>Siyath</title>
+        <link rel="icon" href="images/siyath.jpg" type="image/x-icon">
+        <link rel="stylesheet" href="style.css">
+        
+    </head>
+
+    <body>
+        <div class="logo"><a href="#"><img src="images/logo.jpg" height="30"></a></div>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">Admin</a></li>
+                <li><a href="#">Login</a></li>
+            </ul>
+        </nav>
+
+        <div class="info">
+            <h1>I am <span>Siyath</span>MSM</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore quasi quia veniam <br />
+            vitae delectus doloremque, explicabo sunt fuga cupiditate? Repellendus quam ut<br />
+             eaque accusantium ea, id odio corrupti possimus consectetur.</p>
+            <a href="">Download CV</a>
+        </div>
+        <a href="#" class="download">DOWNLOAD THE APP <img src="images/down-arrow.png"></a>
+
+    </body>
+
+
+</html>
